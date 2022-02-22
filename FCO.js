4524e8db5a700bf1af513a13c7b8443505025851
lelka00777 <lelka00777@gmail.com>
2022-02-23 01:51:56 +0300

    main 23.02

---
+++ FCO.js
@@ -0,0 +1,29 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/selecting-from-many-options-with-switch-statements
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val) {
+  case 1:
+    console.log(answer = 'alpha');
+    break;
+  case 2:
+    console.log(answer ='beta');
+    break;
+  case 3:
+    console.log(answer = 'gamma');
+    break;
+  case 4:
+    console.log(answer ='delta');
+    break;  
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+
+  // Only change code above this line
+
+
+caseInSwitch(1);
